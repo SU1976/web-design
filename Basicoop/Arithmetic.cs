280e73477529be2df4c6c865f746b87b106a71d1
SU1976 <shafiq_urahman@hotmail.co.uk>
2022-04-20 16:25:49 +0100

    added c# code

---
+++ Basicoop/Arithmetic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basicoop
+{
+   public  class Arithmetic:Multiply, IDivision
+    {
+        //member variables
+        int x = 0;
+        int y = 0;
+
+        //Methods
+        public int Add(int a, int b)
+        {
+            x = a;
+            y = b;
+            int sum = x+y;
+            return sum;
+        }
+
+        public float Div(int a, int b)
+        {
+            return a / b;
+        }
+
+        public  int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Basicoop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basicoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            //instantiate
+            Arithmetic arith = new Arithmetic();
+
+            //declaration
+
+            int sum = 0;
+            
+            sum = arith.Add(3, 4);
+            Console.WriteLine("sum is: {0}", sum);
+
+            int product = 0;
+            product = arith.Mult(3, 4);
+            Console.WriteLine("product is: {0}", product);
+
+            float qoutient = 0.0f;
+            qoutient = arith.Div(12, 3);
+            Console.WriteLine("qoutient is: {0}", qoutient);
+
+            Console.ReadLine();
+        }
+    }
+}
